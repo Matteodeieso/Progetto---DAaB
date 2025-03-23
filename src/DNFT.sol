008f812051f38545614dc6c2d9fdc95d3a9bc36d
Matteodeieso <47244362+Matteodeieso@users.noreply.github.com>
2025-03-23 17:21:36 +0100

    update DNFT.sol
    add IPFS cid

--- src/DNFT.sol
+++ src/DNFT.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -8,27 +8,19 @@ interface IWeatherContract {
     function temp() external view returns (uint256);
 }
 
-interface IFunctionsConsumer {
-    function sendRequest(uint64 subscriptionId, string[] calldata args) external returns (bytes32);
-}
-
 contract DNFT is ERC721URIStorage, Ownable {
     IWeatherContract public externalContract;
-    IFunctionsConsumer public functionsConsumer;
     uint256 public tokenIdCounter;
 
-    string private semeURI = "Seme";
-    string private germURI = "Germoglio";
-    string private fioreURI = "Fioritura";
+    string private semeURI = "ipfs://bafkreiea2s4u5vesrbbgbqmbaazvhievo4l4kpzmbdznpyxjnq7nheljie";
+    string private germURI = "ipfs://bafkreidzuplh3cvouuttasdciegsssnnzdvezqceaajoidmgqfmjigmgty";
+    string private fioreURI = "ipfs://bafkreiea2s4u5vesrbbgbqmbaazvhievo4l4kpzmbdznpyxjnq7nheljie";
 
     mapping(uint256 => address) public tokenOwners;
 
-    constructor(address _weatherContract, address _functionsConsumer) 
-        ERC721("DynamicMoodNFT", "DMNFT") Ownable(msg.sender) 
-    {
-        externalContract = IWeatherContract(_weatherContract);
-        functionsConsumer = IFunctionsConsumer(_functionsConsumer);
-    }
+   constructor(address _weatherContract) ERC721("DFlower", "DNFTf") Ownable(msg.sender) {
+    externalContract = IWeatherContract(_weatherContract);
+}
 
     function mintNFT(address recipient) public onlyOwner {
         uint256 newTokenId = tokenIdCounter;
@@ -37,23 +29,19 @@ contract DNFT is ERC721URIStorage, Ownable {
         tokenIdCounter++;
     }
 
-    function updateMood(uint256 tokenId) public {
+    function updateState(uint256 tokenId) public {
         require(ownerOf(tokenId) == msg.sender, "Not the owner of this NFT");
-        uint256 mood = externalContract.temp(); // Ottiene la temperatura
+        uint256 mood = externalContract.temp(); // Prendiamo il valore di temp
         string memory newURI;
 
         if (mood < 10) {
             newURI = semeURI;
-        } else if (mood >= 10 && mood <= 19) {
+        } else if (mood >= 11 && mood <= 19) {
             newURI = germURI;
         } else {
             newURI = fioreURI;
         }
 
         _setTokenURI(tokenId, newURI);
-    }
-
-    function requestWeatherData(uint64 subscriptionId, string[] calldata args) public onlyOwner {
-        functionsConsumer.sendRequest(subscriptionId, args);
     }
 }
